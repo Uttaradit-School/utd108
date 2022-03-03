e167bd17f73a1ae539a8cb61b98f25a8411b1497
Captainistz <mynameisjeff7614@gmail.com>
2022-03-03 19:17:12 +0700

    🧽 clean up

--- pages/friendship/login.tsx
+++ pages/friendship/login.tsx
@@ -58,7 +58,6 @@ const FriendShipAuth: NextPage = () => {
         <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
           <h1 className="text-4xl">UTD Friendship</h1>
           <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-            {/* <Link href="/friendship/details"> */}
             <a
               onClick={login}
               className="mt-6 w-64 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
@@ -68,7 +67,6 @@ const FriendShipAuth: NextPage = () => {
                 เข้าสู่ระบบด้วย Google &rarr;
               </h3>
             </a>
-            {/* </Link> */}
           </div>
         </main>
       </div>
